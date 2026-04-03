f85f35f36b055bc973240ec05851491cc5cab18f
Raphagalv <raphaelgcavalcanti@hotmail.com>
2026-04-02 22:18:16 -0300

    Logica básica dos componentes atribuida

--- main.js
+++ main.js
@@ -10,12 +10,56 @@ buttons.forEach(function (botao) {
 
         if (botao.id === 'clear') {
             // limpa tudo
+            numeroAtual = ''
+            numeroAnterior = ''
+            operacao = ''
+            conta.textContent = ''
+            resultado.textContent = ''
         } else if (botao.id === 'equals') {
-            // calcula
+            // lógica calculos
+            if (numeroAnterior === '' || numeroAtual === '')
+                return
+
+            const num1 = parseFloat(numeroAnterior)
+            const num2 = parseFloat(numeroAtual)
+            let calculo = 0
+
+            if (operacao === '+') calculo = num1 + num2
+            if (operacao === '-') calculo = num1 - num2
+            if (operacao === '*') calculo = num1 * num2
+            if (operacao === '/') calculo = num1 / num2
+
+            conta.textContent = numeroAnterior + ' ' + operacao + ' ' + numeroAtual + ' ='
+            resultado.textContent = calculo
+            numeroAtual = String(calculo)
+            numeroAnterior = ''
+            operacao = ''
         } else if (['+', '-', '*', '/'].includes(valor)) {
-            // guardou operação, número sobe
+            // guarda a operação e sobe numero
+            if (numeroAtual === '' && numeroAnterior === '') return
+
+            if (numeroAtual === '' && operacao !== '') {
+                operacao = valor 
+                conta.textContent = numeroAnterior + ' ' + operacao
+                return
+            }
+
+            operacao = valor
+            numeroAnterior = numeroAtual
+            numeroAtual = ''
+            conta.textContent = numeroAnterior + ' ' + operacao
+            resultado.textContent = ''
         } else {
-            // é um número, adiciona no visor
+            // Se é numero adiciona no visor
+
+            if (valor === ',' && numeroAtual === '') {
+                numeroAtual = '0,'
+            } else if (valor === ',' && numeroAtual.includes(',')) {
+                return
+            } else {
+                numeroAtual += valor
+            }
+            resultado.textContent = numeroAtual
         }
     })
 })
